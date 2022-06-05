fc22d9651803376a141efa8bc6bfa2db35715b6b
unknown <nazeerahmedofficial1.com>
2022-06-05 19:32:07 +0500

    end of task 1

--- fetch_stock_task/src/App.js
+++ fetch_stock_task/src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState,useEffect } from 'react';
 function App() {
+  const [state,setState] = useState(null);
+
+  useEffect(() => {
+    const url = "https://jsonmock.hackerrank.com/api/stocks?date=5-January-2000";
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setState(json.data)
+        console.log(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+}, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+        <div className="App">
+       
+        <table className='table'>
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>Open</th>
+      <th>High</th>
+      <th>Low</th>
+      <th>Close</th>
+    </tr>
+  </thead>
+  <tbody>
+  {state && state.map((val, key) => {
+          return (
+            <tr key={key}>
+               <td>{key}</td>
+              <td>{val.open}</td>
+              <td>{val.high}</td>
+              <td>{val.low}</td>
+              <td>{val.close}</td>
+            </tr>
+          )
+        })}
+  </tbody>
+</table>
+      
     </div>
+    </>
   );
 }
 
